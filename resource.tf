103e9b94c242eac7630edbc4fe0c6990e01c0cf0
gopal <gopal1409@gmail.com>
2021-12-02 16:33:00 +0530

    terraform subnet and nsg

--- resource.tf
+++ resource.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "demoresourcegrp"{
+resource "azurerm_resource_group" "rg"{
     location = var.resource_group_location
     name = "${local.resource_name_prefix}-${var.resource_group_name}-${random_string.myrandom.id}"
     #name = '${var.resource_group_name}-nic'
